63bbc13c743c9bb7ad692cd569cff36b1d49a704
Gioele Moretti <gio.moretti.gio@gmail.com>
2026-02-05 22:36:59 +0100

    fix(crying): Make CryingSession EndTime nullable in migration

--- src/GajaTrack.Infrastructure/Migrations/20260205213537_MakeCryingSessionEndTimeNullable.Designer.cs
+++ src/GajaTrack.Infrastructure/Migrations/20260205213537_MakeCryingSessionEndTimeNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GajaTrack.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GajaTrack.Infrastructure.Migrations
 {
     [DbContext(typeof(GajaDbContext))]
-    partial class GajaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205213537_MakeCryingSessionEndTimeNullable")]
+    partial class MakeCryingSessionEndTimeNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
