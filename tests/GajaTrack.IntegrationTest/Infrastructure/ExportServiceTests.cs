63a77b053adfe74028b0cbbd3677b0802061a9f3
Gioele Moretti <gio.moretti.gio@gmail.com>
2026-02-14 16:34:56 +0100

    refactor: refine test project names and relocate ExportServiceTests

--- tests/GajaTrack.IntegrationTest/Infrastructure/ExportServiceTests.cs
+++ tests/GajaTrack.IntegrationTest/Infrastructure/ExportServiceTests.cs
@@ -5,7 +5,7 @@ using GajaTrack.Infrastructure.Services;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
-namespace GajaTrack.Test.Infrastructure;
+namespace GajaTrack.IntegrationTest.Infrastructure;
 
 public class ExportServiceTests : IDisposable
 {
