566ad7b8226be1ed092f1cf38cffa360b0257907
Gioele Moretti <gio.moretti.gio@gmail.com>
2026-02-15 15:34:29 +0100

    app: remove unnecessary interfaces for local services

--- src/GajaTrack.Application/ApplicationServiceRegistration.cs
+++ src/GajaTrack.Application/ApplicationServiceRegistration.cs
@@ -2,7 +2,6 @@ using GajaTrack.Application.Interfaces;
 using GajaTrack.Application.Queries;
 using GajaTrack.Application.Services;
 using GajaTrack.Domain;
-using GajaTrack.Domain.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GajaTrack.Application;
@@ -11,13 +10,12 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddScoped<DailyRhythmMapDomainService>();
         services.AddScoped<IDailyRhythmMapService, DailyRhythmMapService>();
         
-        // BabyDay Services
-        services.AddScoped<ICalculateSleep, CalculateSleep>();
-        services.AddScoped<ICountWakings, CountWakings>();
-        services.AddScoped<IGetBabyDayQuery, GetBabyDayQuery>();
+        // BabyDay Services (KISS: No interfaces needed for internal domain logic)
+        services.AddScoped<CalculateSleep>();
+        services.AddScoped<CountWakings>();
+        services.AddScoped<GetBabyDay.Execution>();
         
         return services;
     }
